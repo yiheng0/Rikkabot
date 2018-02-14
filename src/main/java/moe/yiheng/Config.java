21f610a65155e6f46e5b03ce76972ab5c52913bb
yiheng0 <yiheng0510@outlook.com>
2018-02-14 17:03:59 +0800

    Delete Config.java
    应该 修复了 跟Kacyan bot指令冲突的bug

--- src/main/java/moe/yiheng/Config.java
+++
@@ -1,6 +0,0 @@
-package moe.yiheng;
-
-public class Config {
-    public static String token = "507745437:AAHB87ZWIyXuj0oU9IQT4okDMwMqHO84uuQ";
-    public static String botName = "rikka_as_bot";
-}
